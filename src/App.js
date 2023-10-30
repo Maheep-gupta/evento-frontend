0cb6ed43607d20fe997283e2a2f1115e25409b07
Maheep Gupta <91566211+CodingStuffsofMahee@users.noreply.github.com>
2023-10-30 15:13:56 +0530

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './comopnents/Login';
+import Signup from './comopnents/Signup';
 
 function App() {
   return (
     <>
-      <Login/>
+      <Login />
+      <Signup/>
     </>
   );
 }
--- src/comopnents/Signup.js
+++ src/comopnents/Signup.js
@@ -1,4 +1,4 @@
-export default function Login() {
+export default function Signup() {
     return (
       <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
