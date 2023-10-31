dc8a9b3a662e77995d888a62594ffe743bd95d0c
Maheep Gupta <91566211+CodingStuffsofMahee@users.noreply.github.com>
2023-10-31 23:26:19 +0530

    bugs fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,20 +8,20 @@ function Navbar({ title }) {
     console.log(title);
     return (
         <>
-            <div class="flex flex-col items-center w-48 h-screen overflow-hidden text-gray-700 bg-gray-100 rounded">
-                <Link class="flex items-center w-full px-3 mt-3" to="/">
-                    <div class=" h-10 fill-current">
+            <div className="flex flex-col items-center w-48 h-screen overflow-hidden text-gray-700 bg-gray-100 rounded">
+                <Link className="flex items-center w-full px-3 mt-3" to="/">
+                    <div className=" h-10 fill-current">
                         <img src={require('../assets/evento-black.png')} alt='EventO'/>
                     </div>
                 </Link>
                 {console.log(admin)}
                 {admin ? <AdminNav title={title} /> : <UserNav title={title } />}
                 {/* Account */}
-                <Link class="flex items-center justify-center w-full h-16 mt-auto bg-gray-200 hover:bg-gray-300 text-black" to="#">
-                    <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <Link className="flex items-center justify-center w-full h-16 mt-auto bg-gray-200 hover:bg-gray-300 text-black" to="#">
+                    <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <span class="ml-2 text-sm font-medium">Account</span>
+                    <span className="ml-2 text-sm font-medium">Account</span>
                 </Link>
             </div>
         </>
